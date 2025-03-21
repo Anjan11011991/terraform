fbcd8289585e19bc7ff64f347fe49daf0494a9dd
ANJAN <devopsanjan700@gmail.com>
2025-03-21 11:50:33 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,34 @@
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = ["${aws_security_group.sg.id}"]
 
+  tags = {
+    Name = "Terraform-EC2-Instance"
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name_prefix = "tf-ec2-"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform-SG"
+  }
+}
